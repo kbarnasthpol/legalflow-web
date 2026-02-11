dd3b4977f01652f119e697d7e1d8273102e4b4d3
Kevin Barnasthpol <kevin.barnasthpol@gmail.com>
2026-02-11 15:59:15 -0300

    Agregado de tipografia Google Font via next/font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,22 @@
 'use client' // Lo convertimos a Client Component para usar el hook
 import { usePathname } from 'next/navigation'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { Lexend } from 'next/font/google'; // Cambia Montserrat por la que elijas
 import "./globals.css"
 import Sidebar from '@/components/ui/Sidebar'
+
+const myFont = Lexend({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-primary', // Creamos una variable CSS
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname()
   const isLoginPage = pathname === '/login'
 
   return (
-    <html lang="es">
+    <html lang="es" className={myFont.variable}>
       <body className="flex">
         {/* Envolvemos TODA la app con el Provider de Google */}
         <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
